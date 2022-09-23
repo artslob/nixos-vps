34e0ffd1f1f17d7f2f484e025212a960080c212c
artslob <artslob@yandex.ru>
2022-09-23 20:05:45 +0000

    update ssh config

--- configuration.nix
+++ configuration.nix
@@ -101,11 +101,12 @@
   #   enableSSHSupport = true;
   # };
 
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
-  services.openssh.permitRootLogin = "yes";
+  services.openssh = {
+    enable = true;
+    permitRootLogin = "yes";
+    passwordAuthentication = false;
+    kbdInteractiveAuthentication = false;
+  };
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
