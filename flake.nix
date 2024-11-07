edd7e40317c33fa90c05b0d4c9ba5a4f074c8abc
artslob <artslob@yandex.ru>
2024-11-07 22:27:39 +0700

    add agenix as module

--- flake.nix
+++ flake.nix
@@ -4,6 +4,12 @@
   inputs = {
     # NixOS official package source, using the nixos-23.11 branch here
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+
+    agenix = {
+      url = "github:ryantm/agenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.darwin.follows = "";
+    };
   };
 
   outputs = { self, nixpkgs, ... }@inputs: {
@@ -13,6 +19,7 @@
         # Import the previous configuration.nix we used,
         # so the old configuration file still takes effect
         ./configuration.nix
+        inputs.agenix.nixosModules.default
       ];
     };
   };
