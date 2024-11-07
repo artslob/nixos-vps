fe672e12613d58705d8de69f5252a77ffc6df12f
artslob <artslob@yandex.ru>
2024-11-07 23:58:22 +0700

    add nix shell for development

---
+++ dev.nix
@@ -0,0 +1,14 @@
+{ pkgs ? import (fetchTarball
+  "https://github.com/NixOS/nixpkgs/archive/refs/tags/24.05.tar.gz") { }
+, agenix ?
+  (builtins.fetchTarball "https://github.com/ryantm/agenix/archive/main.tar.gz")
+}:
+
+pkgs.mkShell {
+  buildInputs = with pkgs; [
+    nixos-rebuild
+    (pkgs.callPackage "${agenix}/pkgs/agenix.nix" { })
+  ];
+
+  nativeBuildInputs = with pkgs; [ pkg-config ];
+}
