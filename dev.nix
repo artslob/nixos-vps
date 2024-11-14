79b223b01d0d3b48bd9918780824b933e42361dd
artslob <artslob@yandex.ru>
2024-11-14 22:55:54 +0700

    use commit for agenix

--- dev.nix
+++ dev.nix
@@ -1,8 +1,10 @@
 { pkgs ? import (fetchTarball
   "https://github.com/NixOS/nixpkgs/archive/refs/tags/24.05.tar.gz") { }
-, agenix ?
-  (builtins.fetchTarball "https://github.com/ryantm/agenix/archive/main.tar.gz")
-}:
+, agenix ? (builtins.fetchTarball {
+  url =
+    "https://github.com/ryantm/agenix/archive/f6291c5935fdc4e0bef208cfc0dcab7e3f7a1c41.tar.gz";
+  sha256 = "1x8nd8hvsq6mvzig122vprwigsr3z2skanig65haqswn7z7amsvg";
+}) }:
 
 pkgs.mkShell {
   buildInputs = with pkgs; [
