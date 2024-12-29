6cc830f010b1eb389577f4a5799cb0870aa096cc
artslob <artslob@yandex.ru>
2024-12-29 20:37:28 +0700

    enable replace and ephemeral

--- configuration.nix
+++ configuration.nix
@@ -187,7 +187,8 @@
       enable = true;
       name = "test-github-runner";
       user = "github-runner";
-      # replace = true;
+      replace = true;
+      ephemeral = true;
       tokenFile = config.age.secrets."test-github-runner-token".path;
       url = "https://github.com/artslob/test-github-runner";
       extraPackages = with pkgs; [ curl wget rustup gcc docker_27 ];
