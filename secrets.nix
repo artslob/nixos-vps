625e57d0c37ff58ef0bd99318612913eedd8cf26
artslob <artslob@yandex.ru>
2025-03-12 23:31:39 +0700

    use -r to re-encrypts all secrets with new keys

--- secrets.nix
+++ secrets.nix
@@ -4,7 +4,13 @@ let
   personal_projects =
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICFNSePweS1I3ucce46M50qXbr0330RUOMG9ZiGchvxN";
 
-  all = [ vps personal_projects ];
+  all = [
+    vps
+    personal_projects
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBiAvme/Pup3RUJRZIrQAfUqVH0XGAmr173XHtYeF669"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGJLGER9xJoAmr0FjphcectmJRyMwRuZodFVHgm4INq9"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBNx5a3WMd3VVY42Gtz2rUvb+8WK8yZjsk5O3q4TqRmD"
+  ];
 in {
   "secrets/secret1.age".publicKeys = all;
   "secrets/test-github-runner-token.age".publicKeys = all;
