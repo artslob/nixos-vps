0e0c4cb930d7d7fa8706283105a1fb78a95d77bc
artslob <artslob@yandex.ru>
2025-03-16 11:35:12 +0700

    refactor urls

--- dev.nix
+++ dev.nix
@@ -1,12 +1,9 @@
-let
-  pkgs_url = "https://github.com/NixOS/nixpkgs/archive/refs/tags/24.05.tar.gz";
-  agenix_url =
-    "https://github.com/ryantm/agenix/archive/f6291c5935fdc4e0bef208cfc0dcab7e3f7a1c41.tar.gz";
-in { pkgs ? import (fetchTarball {
-  url = pkgs_url;
+{ pkgs ? import (fetchTarball {
+  url = "https://github.com/NixOS/nixpkgs/archive/refs/tags/24.05.tar.gz";
   sha256 = "1lr1h35prqkd1mkmzriwlpvxcb34kmhc9dnr48gkm8hh089hifmx";
 }) { }, agenix ? (fetchTarball {
-  url = agenix_url;
+  url =
+    "https://github.com/ryantm/agenix/archive/f6291c5935fdc4e0bef208cfc0dcab7e3f7a1c41.tar.gz";
   sha256 = "1x8nd8hvsq6mvzig122vprwigsr3z2skanig65haqswn7z7amsvg";
 }) }:
 
