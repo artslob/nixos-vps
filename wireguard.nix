fd840c4c3bc6e1edd602f43a10670746cc3389cc
artslob <artslob@yandex.ru>
2025-05-03 11:57:21 +0300

    use wg-quick

--- wireguard.nix
+++ wireguard.nix
@@ -1,35 +1,48 @@
 { config, lib, pkgs, ... }: {
-  networking.nat.enable = true;
-  networking.nat.externalInterface = "ens18";
-  networking.nat.internalInterfaces = [ "wg0" ];
-  networking.nat.enableIPv6 = true;
-  networking.firewall = { allowedUDPPorts = [ 51820 ]; };
+  networking.nat = {
+    enable = true;
+    externalInterface = "ens18";
+    internalInterfaces = [ "wg0" ];
+    enableIPv6 = true;
+  };
+  networking.firewall = {
+    allowedTCPPorts = [ 53 ];
+    allowedUDPPorts = [ 53 51820 ];
+  };
   networking.enableIPv6 = true;
   boot.kernel.sysctl = { "net.ipv6.conf.all.forwarding" = "1"; };
+  services.dnsmasq = {
+    enable = true;
+    settings.interface = "wg0";
+  };
 
   age.secrets."wireguard-private-key".file =
     ./secrets/wireguard-private-key.age;
 
-  networking.wireguard.interfaces = {
+  networking.wg-quick.interfaces = {
     # "wg0" is the network interface name. You can name the interface arbitrarily.
     wg0 = {
       # Determines the IP address and subnet of the server's end of the tunnel interface.
-      ips = [ "10.100.0.1/24" "fd42:42:42::1/64" ];
+      address = [ "10.0.0.1/24" "fdc9:281f:04d7:9ee9::1/64" ];
 
       # The port that WireGuard listens to. Must be accessible by the client.
       listenPort = 51820;
 
       # This allows the wireguard server to route your traffic to the internet and hence be like a VPN
       # For this to work you have to set the dnsserver IP of your router (or dnsserver of choice) in your clients
-      postSetup = ''
-        ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s 10.100.0.0/24 -o ens18 -j MASQUERADE
-        ${pkgs.iptables}/bin/ip6tables -t nat -A POSTROUTING -s fd42:42:42::/64 ! -d fd42:42:42::/64 -j MASQUERADE
+      postUp = ''
+        ${pkgs.iptables}/bin/iptables -A FORWARD -i wg0 -j ACCEPT
+        ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s 10.0.0.1/24 -o ens18 -j MASQUERADE
+        ${pkgs.iptables}/bin/ip6tables -A FORWARD -i wg0 -j ACCEPT
+        ${pkgs.iptables}/bin/ip6tables -t nat -A POSTROUTING -s fdc9:281f:04d7:9ee9::1/64 -o ens18 -j MASQUERADE
       '';
 
       # This undoes the above command
-      postShutdown = ''
-        ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s 10.100.0.0/24 -o ens18 -j MASQUERADE
-        ${pkgs.iptables}/bin/ip6tables -t nat -D POSTROUTING -s fd42:42:42::/64 ! -d fd42:42:42::/64 -j MASQUERADE
+      preDown = ''
+        ${pkgs.iptables}/bin/iptables -D FORWARD -i wg0 -j ACCEPT
+        ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s 10.0.0.1/24 -o ens18 -j MASQUERADE
+        ${pkgs.iptables}/bin/ip6tables -D FORWARD -i wg0 -j ACCEPT
+        ${pkgs.iptables}/bin/ip6tables -t nat -D POSTROUTING -s fdc9:281f:04d7:9ee9::1/64 -o ens18 -j MASQUERADE
       '';
 
       # Path to the private key file.
@@ -46,7 +59,7 @@
           # Public key of the peer (not a file path).
           publicKey = "2wrhPpl3w6BM06BzNQRkgGMmIOwGaEFLMImGZY9iYS8=";
           # List of IPs assigned to this peer within the tunnel subnet. Used to configure routing.
-          allowedIPs = [ "10.100.0.2/32" "fd42:42:42::2/128" ];
+          allowedIPs = [ "10.0.0.2/32" "fdc9:281f:04d7:9ee9::2/128" ];
         }
       ];
     };
